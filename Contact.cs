d187a468b957a662db11b8ae67cf919f1d5728be
Aakankshap16 <aakankshap971@gmail.com>
2023-03-22 14:11:10 +0530

    Contact list overidden to string

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBookSystem
+{
+   public class Contact
+    {
+        public String firstname, lastname, mobilenumber, address, city, zip, state, email;
+        public override string ToString()
+        {
+            return $"First Name: {firstname}, Last Name: {lastname}, Mobile Number: {mobilenumber}, Address: {address}, City: {city}, State: {state}, Zip: {zip}, Email: {email}";
+        }
+
+    }
+}
