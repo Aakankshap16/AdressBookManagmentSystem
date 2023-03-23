7afb1a37a02f6bef4b8a75efa40bfd74a9c7bed6
Aakanksha pandey <125583605+Aakankshap16@users.noreply.github.com>
2023-03-23 17:36:44 +0530

    Ability to edit the exisiting address book

--- AddressBookMainClass.cs
+++ AddressBookMainClass.cs
@@ -1,11 +1,10 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
+using system;
 namespace AdressBookSystem
 {
     class AddressBookMainClass
     {
         List<Contact> con = new List<Contact>();
-        Contact obj = new Contact(); 
+        Contact obj = new Contact();
         public void AddDetails()
         {
             Console.WriteLine("Enter First Name: ");
@@ -39,6 +38,7 @@ namespace AdressBookSystem
 
             foreach (var change in con)
             {
+                Console.WriteLine("The details stored by you in AdressBookSystem book");
                 Console.WriteLine(change.ToString());
             }
 
@@ -49,81 +49,88 @@ namespace AdressBookSystem
             Console.WriteLine("Enter the name of the person for update:");
             string name = Console.ReadLine();
 
-            Console.WriteLine("For update:");
-            Console.WriteLine("a)First Name \n b)LastName \n c)MobileNumber \n d) Address \n e)City \n f)State \n g)Zip \n h)EmailId");
-            char update= Convert.ToChar(Console.ReadLine());
-            switch (update)
+            foreach (var data in con)
             {
-                case 'a':
+                if (data.firstName == name)
+                {
+
+                    Console.WriteLine("For update:");
+                    Console.WriteLine("a)First Name \n b)LastName \n c)MobileNumber \n d) Address \n e)City \n f)State \n g)Zip \n h)EmailId");
+                    char update = Convert.ToChar(Console.ReadLine());
+                    switch (update)
                     {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String firstName = value;
-                        Console.WriteLine("First Name updated...");
-                        break;
+                        case 'a':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.firstName = value;
+                                Console.WriteLine("First Name updated...");
+                                break;
+                            }
+                        case 'b':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.lastName = value;
+                                Console.WriteLine("Last name updated...");
+                                break;
+                            }
+                        case 'c':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.mobileNumber = value;
+                                Console.WriteLine(" Mobile No updated...");
+                                break;
+                            }
+                        case 'd':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.address = value;
+                                Console.WriteLine("Address updated....");
+                                break;
+                            }
+                        case 'e':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.city = value;
+                                Console.WriteLine("City updated...");
+                                break;
+                            }
+                        case 'f':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.state = value;
+                                Console.WriteLine("State updated...");
+                                break;
+                            }
+                        case 'g':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.zip = value;
+                                Console.WriteLine("Zip updated....");
+                                break;
+                            }
+                        case 'h':
+                            {
+                                Console.WriteLine("Please Enter the new value:");
+                                string value = Console.ReadLine();
+                                data.email = value;
+                                Console.WriteLine("Emailid updated...");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("NO Changes");
+                                break;
+                            }
                     }
-                case 'b':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String lastName = value;
-                        Console.WriteLine("Last name updated...");
-                        break;
-                    }
-                case 'c':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String mobileNumber = value;
-                        Console.WriteLine(" Mobile No updated...");
-                        break;
-                    }
-                case 'd':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String address = value;
-                        Console.WriteLine("Address updated....");
-                        break;
-                    }
-                case 'e':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String city = value;
-                        Console.WriteLine("City updated...");
-                        break;
-                    }
-                case 'f':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String state = value;
-                        Console.WriteLine("State updated...");
-                        break;
-                    }
-                case 'g':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String zip = value;
-                        Console.WriteLine("Zip updated....");
-                        break;
-                    }
-                case 'h':
-                    {
-                        Console.WriteLine("Please Enter the new value:");
-                        string value = Console.ReadLine();
-                        String email = value;
-                        Console.WriteLine("Emailid updated...");
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("NO Changes");
-                        break;
-                    }
-            
+                }
+
             }
         }
         public static void Main(string[] args)
@@ -132,41 +139,49 @@ namespace AdressBookSystem
             Console.WriteLine("!!!!!!Welcome to Adress Book Program!!!!!!");
             Console.WriteLine(" ");
             AddressBookMainClass person = new AddressBookMainClass();
-            Console.WriteLine("Do you want to add new contact YES(1) or NO(0)");
-            int  option = int.Parse(Console.ReadLine());
-          
-            switch (option)
-            { 
-                case 1:
-                {
-                    Console.WriteLine("Please add new contact details: ");
-                    person.AddDetails();
-                    person.DisplayContact();
-                    break;
-                }
-                case 0:
-                {
-                        Console.WriteLine("Do you Want to Edit your Contact YES(1) or NO(0):");
-                        int option1 = int.Parse(Console.ReadLine());
-                                switch (option1)
-                                {
-                                    case 1:
-                                        {                                           
-                                            person.EditContact();
-                                            break;
-                                        }
-                                    case 0:
-                                        {
-                                            Console.WriteLine("press any key for exit:");
-                                            break;
-                                        }
-                                }
-                        break;
-                }
+            bool flag = true;
+            while (flag == true)
+            {
+                Console.WriteLine("Do you want to add new contact YES(1) or NO(0)");
+                int option = int.Parse(Console.ReadLine());
 
+                switch (option)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Please add new contact details: ");
+                            person.AddDetails();
+                            person.DisplayContact();
+                            break;
+                        }
+                    case 0:
+                        {
+                            Console.WriteLine("Do you Want to Edit your Contact YES(1) or NO(0):");
+                            int option1 = int.Parse(Console.ReadLine());
+                            switch (option1)
+                            {
+                                case 1:
+                                    {
+                                        person.EditContact();
+                                        person.DisplayContact();
+                                        flag = false;
+                                        break;
+
+                                    }
+                                case 0:
+                                    {
+                                        Console.WriteLine("press any key for exit:");
+                                        flag = false;
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+
+
+
+                }
             }
         }
-
-
     }
 }
